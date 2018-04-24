d6c186ab6577a6e493ff20c9b61b24fe39e78b00
JORGE GUADARRAMA DEL ANGEL <30603738+GuadarramaDelAngel-Jorge@users.noreply.github.com>
2018-04-24 18:34:08 -0500

    Update Settings.swift

--- ChangeN/ChangeMyColorsNotMyMind/ChangeMyColorsNotMyMind/Template/Settings.swift
+++ ChangeN/ChangeMyColorsNotMyMind/ChangeMyColorsNotMyMind/Template/Settings.swift
@@ -9,14 +9,20 @@
 import SpriteKit
 
 // Always declared as UInt32
+// Avoid creating instances of my type
 enum PhysicsCategories {
-    
+ // Type properties
     static let none:UInt32 = 0
-    static let ballCategory: UInt32 = 0x1 // 1
+    // Physics categories in SpriteKit are always declared as UInt32. Unsigned 32 bit int
+    // We don't need any physical simulation to take place
+    
+    // Different way to write 1
+    static let ballCategory: UInt32 = 0x1 // 01
     
     //bitwise shift operator
     static let switchCategory: UInt32 = 0x1 << 1 //10
     // Shift all my bits
+    // This prepares my masks to compare each digit
     
 }
 
