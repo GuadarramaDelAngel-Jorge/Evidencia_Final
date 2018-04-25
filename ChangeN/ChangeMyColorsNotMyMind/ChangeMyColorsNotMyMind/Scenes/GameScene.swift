4631c6c19ac3d29b9e92f8e7790f5ddf3b768900
JORGE GUADARRAMA DEL ANGEL <30603738+GuadarramaDelAngel-Jorge@users.noreply.github.com>
2018-04-24 19:19:14 -0500

    Update GameScene.swift

--- ChangeN/ChangeMyColorsNotMyMind/ChangeMyColorsNotMyMind/Scenes/GameScene.swift
+++ ChangeN/ChangeMyColorsNotMyMind/ChangeMyColorsNotMyMind/Scenes/GameScene.swift
@@ -30,9 +30,13 @@ enum SwitchState: Int{
 class GameScene: SKScene {
     
     var colorSwitch: SKSpriteNode!
+    // Defining an initial state to the image 
+    // Red upwards
     var switchState = SwitchState.red
+    // Optional
     var currentColorIndex:Int?
     
+    // For the menu
     let scoreLabel = SKLabelNode(text:"0")
     var score = 0
     
@@ -48,9 +52,11 @@ class GameScene: SKScene {
         
         // We could setup the value of Earth's gravity but it's way too big for a value and for its playability
         physicsWorld.gravity = CGVector(dx: 0.0, dy: -0.5)
+        // Register the ocurring physics contact
         physicsWorld.contactDelegate = self
     }
     
+    // Changing the gravity to -1.0
     func setupChangePhysics(){
         
         physicsWorld.gravity = CGVector(dx: 0.0, dy: -1.0)
@@ -58,6 +64,7 @@ class GameScene: SKScene {
         
     }
     
+    // Changing the gravity again 
     func setupChangePhysicsAgain(){
         
         physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
@@ -74,8 +81,13 @@ class GameScene: SKScene {
         // In order to avoid letting the colorSwitch stick to the bottom
         colorSwitch.position = CGPoint(x: frame.midX, y: frame.minY + colorSwitch.size.height)
         colorSwitch.zPosition = ZPositions.colorSwitch
+        // Defining physics
+        // This mirrors the shape of the colorSwitch image
         colorSwitch.physicsBody = SKPhysicsBody(circleOfRadius:colorSwitch.size.width/2)
+        // Defining the category of the mask
+        // UInt32
         colorSwitch.physicsBody?.categoryBitMask = PhysicsCategories.switchCategory
+        // Stops its physicsbody in order to avoid get affected by other forces
         colorSwitch.physicsBody?.isDynamic = false
         // Pass the node
         addChild(colorSwitch)
@@ -97,10 +109,12 @@ class GameScene: SKScene {
     }
     
     func spawnBall(){
+        // The way to define a random int between 0 and 3
         currentColorIndex = Int(arc4random_uniform(UInt32(4)))
-        
+        // Texture (normal image), color and size
         let ball = SKSpriteNode(texture: SKTexture(imageNamed: "Ball"), color: PlayColors.colors[currentColorIndex!], size:CGSize(width: 30.0, height: 30.0))
         // ball.size = CGSize(width: 30.0, height: 30.0)
+        // Color applied to my texture
         ball.colorBlendFactor = 1.0
         ball.name = "Ball"
         // Up
@@ -108,19 +122,26 @@ class GameScene: SKScene {
         ball.zPosition = ZPositions.ball
         ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width/2)
         ball.physicsBody?.categoryBitMask = PhysicsCategories.ballCategory
+        // Determines when does the ball touches the switch
         ball.physicsBody?.contactTestBitMask = PhysicsCategories.switchCategory
+        // Determines whether items can collide with each other
+        // We don't want the ball to collide but to chech their interaction
         ball.physicsBody?.collisionBitMask = PhysicsCategories.none
         addChild(ball)
     }
     
+    // Method to move the image by taps
     func turnColorWheel(){
         
+        // Avoid nil
         if let newState = SwitchState(rawValue: switchState.rawValue+1){
             switchState = newState
         } else {
+            // First case in the enum
             switchState = .red
         }
         
+        // Defining the animation of the node
         colorSwitch.run(SKAction.rotate(byAngle: .pi/2, duration: 0.25))
         
     }
@@ -144,18 +165,22 @@ class GameScene: SKScene {
     }
 }
 
+// This only implements optional methods
 extension GameScene: SKPhysicsContactDelegate {
     
     func didBegin(_ contact: SKPhysicsContact){
         //01
         //10
         //11
+        // contact has the physical bodies of the nodes
         let contactMask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+        // React to the contact between the ball and the colorSwitch
         if contactMask == PhysicsCategories.ballCategory | PhysicsCategories.switchCategory {
             // TEST print("Collision")
-            
+            // Check the node and asign the name Ball
             if let ball = contact.bodyA.node?.name == "Ball" ? contact.bodyA.node as? SKSpriteNode : contact.bodyB.node as? SKSpriteNode {
                 
+                // Check colors
                 if currentColorIndex == switchState.rawValue {
                     // print("Correct function")
                     score+=1
@@ -168,8 +193,11 @@ extension GameScene: SKPhysicsContactDelegate {
                     }
                     
                     updateScoreLabel()
+                    // As soon as the color matches the wheel
                     ball.run(SKAction.fadeOut(withDuration: 0.05), completion: {
+                        // Remove the old ball
                         ball.removeFromParent()
+                        // Add new ball
                         self.spawnBall()
                     })
                 }else {
